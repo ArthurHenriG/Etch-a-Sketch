88b4453422d18b4420e8f21d542c30bf5d21fac8
ArthurHenriG <ArthurHenri00@outlook.com>
2025-02-17 15:41:58 -0300

    Add createGrid function. The function takes input and generates a grid of square divs, using two for loops to create the rows and columns.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const container=document.querySelector('.container');
+
+function createGrid(number){
+ 
+   for(let i=0;i<number;i++){
+     for(let j=0;j<number;j++){
+        const div=document.createElement('div');
+        container.appendChild(div);
+        div.style.width='50px';
+        div.style.height='50px';
+        div.style.backgroundColor='red';
+        div.style.border='solid 2px black';
+     }
+   }
+
+}
+
+createGrid(16);
