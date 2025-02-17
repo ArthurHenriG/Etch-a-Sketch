d369053a77e5bfb88a2bddaa54a23884832dedca
ArthurHenriG <ArthurHenri00@outlook.com>
2025-02-17 19:48:25 -0300

    Add event listener to a button that function get user input, clear container div and generate a new grid with user input

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const container=document.querySelector('.container');
+const button=document.querySelector('button');
+let userInput;
 
+
+//GET USER INPUT, CLEAN THE CONTAINER DIV AND GENERATE A NEW GRID
+button.addEventListener('click',()=>{
+    userInput=window.prompt('Type the number of squares');
+    container.innerHTML='';
+    createGrid(userInput);
+})
+
+
+//CREATE A NEW GRID 
 function createGrid(number){
- 
    for(let i=0;i<number;i++){
      for(let j=0;j<number;j++){
         const div=document.createElement('div');
         container.appendChild(div);
-        div.style.width='50px';
-        div.style.height='50px';
-        div.style.backgroundColor='red';
-        div.style.border='solid 2px black';
+        div.classList.add('child');
      }
    }
 
 }
-
-createGrid(16);
